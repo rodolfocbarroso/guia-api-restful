e8bff7d9042aa233f1d573dde27dbbe550585694
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-11-05 00:17:31 -0300

    ✅ Adiciona model de categoria

---
+++ GuiaApiRestful/Models/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuiaApiRestful.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MaxLength(60, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        public string Titulo { get; set; }
+    }
+}
