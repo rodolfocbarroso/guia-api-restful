6159dcc7d403e9ad65490642c2a09c02c9d81218
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-11-05 01:10:26 -0300

    ✅ Adiciona configuracao do data context na classe startup

--- GuiaApiRestful/Startup.cs
+++ GuiaApiRestful/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GuiaApiRestful.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,11 @@ namespace GuiaApiRestful
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("Default")));
+
+            services.AddScoped<DataContext, DataContext>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
