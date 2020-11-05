3f71d26fb219a2855d0de80029ba2b71eab2b857
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-11-05 00:35:30 -0300

    ✅ Adiciona data context

---
+++ GuiaApiRestful/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GuiaApiRestful.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuiaApiRestful.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categories { get; set; }
+    }
+}
