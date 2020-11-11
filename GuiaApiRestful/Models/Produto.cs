ed72e886d5d095048d987bb9b1bfd20f1ebba56f
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-11-10 23:06:12 -0300

    ✅ Adiciona model de produto

---
+++ GuiaApiRestful/Models/Produto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuiaApiRestful.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MaxLength(60, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        public string Nome { get; set; }
+
+        [MaxLength(1024, ErrorMessage = "Este campo deve conter no máximo 1024 caracteres")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero")]
+        public decimal Preco { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "Categoria inválida")]
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
