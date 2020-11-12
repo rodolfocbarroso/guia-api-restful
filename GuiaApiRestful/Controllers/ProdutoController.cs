c7b66efc8c8461fb815c70c49f62c06b9f7a27eb
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-11-11 23:53:41 -0300

    ✅ Adiciona metodo get no controller de produto

---
+++ GuiaApiRestful/Controllers/ProdutoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GuiaApiRestful.Data;
+using GuiaApiRestful.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuiaApiRestful.Controllers
+{
+    [Route("produtos")]
+    public class ProdutoController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Produto>>> Get([FromServices] DataContext context)
+        {
+            var produtos = await context.Produtos.AsNoTracking().ToListAsync();
+
+            return Ok(produtos);
+        }
+
+    }
+}
