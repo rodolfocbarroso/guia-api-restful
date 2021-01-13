29099ff4c964f6cdbe060208df4ee77013b3b441
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2021-01-12 22:22:43 -0300

    🔧 Adiciona class settings para o secret do jwt

---
+++ GuiaApiRestful/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuiaApiRestful
+{
+    public static class Settings
+    {
+        public static string Secret = "4f9c6887380f4362b4f3eed3f54d8059";
+    }
+}
