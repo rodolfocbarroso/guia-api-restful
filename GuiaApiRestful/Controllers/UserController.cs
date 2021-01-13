ed83938785a063d76e2f3ff16edd5b9e743de41e
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2021-01-12 23:41:39 -0300

    ✅ Adiciona metodo post no controller de user

---
+++ GuiaApiRestful/Controllers/UserController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GuiaApiRestful.Data;
+using GuiaApiRestful.Models;
+using GuiaApiRestful.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuiaApiRestful.Controllers
+{
+    [Route("users")]
+    public class UserController : Controller
+    {
+        [HttpPost]
+        [Route("")]
+        [AllowAnonymous]
+        public async Task<ActionResult<User>> Post([FromServices] DataContext context, [FromBody] User model)
+        {
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+
+                await context.Users.AddAsync(model);
+                await context.SaveChangesAsync();
+
+                model.Password = "";
+
+                return model;
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível criar o usuário" });
+
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<dynamic>> Authenticate([FromServices] DataContext context, [FromBody] User model)
+        {
+            var user = await context.Users
+                .AsNoTracking()
+                .Where(x => x.Username == model.Username && x.Password == model.Password)
+                .FirstOrDefaultAsync();
+
+            if (user == null)
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+
+            var token = TokenService.GenerateToken(user);
+            
+            user.Password = "";
+
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+    }
+}
